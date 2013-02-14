27a86828d897b36dcf6d6256ecb248ad8b9c996a
Stuart <lodge.stuart@gmail.com>
2013-02-14 10:19:44 +0000

    refactoring - separate out the listeners

---
+++ MVVMEmiExtensions/EmiTimeChangedListener.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Android.Widget;
+
+namespace MVVMEmiExtensions
+{
+    public class EmiTimeChangedListener : Java.Lang.Object, TimePicker.IOnTimeChangedListener
+    {
+        private EmiTimePicker timePicker;
+
+        public EmiTimeChangedListener(EmiTimePicker timePicker)
+        {
+            this.timePicker = timePicker;
+        }
+
+        public void OnTimeChanged(TimePicker view, int hourOfDay, int minute)
+        {
+            timePicker.InternalSetValueAndRaiseChanged(new TimeSpan(hourOfDay, minute, 0));
+        }
+    }
+}
